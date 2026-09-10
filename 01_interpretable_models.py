40e374295be5a5012c69b9d126588eb9ff14eb2d
farhadmonavar <farhad.monavar@gmail.com>
2026-09-10 17:00:54 +0330

    feat(xai): add local and global explanations for logistic regression model

--- 01_interpretable_models.py
+++ 01_interpretable_models.py
@@ -1,6 +1,10 @@
 # %% Imports
 from utils import DataLoader
-
+from interpret.glassbox import (LogisticRegression,
+                                ClassificationTree,
+                                ExplainableBoostingClassifier)
+from interpret import show
+from sklearn.metrics import f1_score, accuracy_score
 
 # %% Load and preprocess data
 data_loader = DataLoader()
@@ -14,3 +18,20 @@ print(X_test.shape)
 # Oversample the train data
 X_train, y_train = data_loader.oversample(X_train, y_train)
 print("After oversampling:", X_train.shape)
+# %% Fit logistic regression model
+lr = LogisticRegression(random_state=2021, feature_names=X_train.columns, penalty='l1', solver='liblinear')
+lr.fit(X_train, y_train)
+print("Training finished.")
+
+# %% Evaluate logistic regression model
+y_pred = lr.predict(X_test)
+print(f"F1 Score {f1_score(y_test, y_pred, average='macro')}")
+print(f"Accuracy {accuracy_score(y_test, y_pred)}")
+
+# %% Explain local prediction
+lr_local = lr.explain_local(X_test[:100], y_test[:100], name='Logistic Regression')
+show(lr_local)
+
+# %% Explain global logistic regression model
+lr_global = lr.explain_global(name='Logistic Regression')
+show(lr_global)
